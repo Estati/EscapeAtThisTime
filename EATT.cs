b3277eafada1cbcd8b3865bca29a5e7689ecaed0
josep <josep@Desktop-1SW5K>
2024-04-19 19:24:47 -0500

    Add project files.

---
+++ EATT.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using MelonLoader;
+
+namespace EscapeAtThisTime
+{
+    public class EATT : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                LoadMainMenuScene();
+            }
+        }
+
+        private void LoadMainMenuScene()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("sl_mainmenu");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using MelonLoader;
+using EscapeAtThisTime; // The namespace of your mod class
+// ...
+[assembly: MelonInfo(typeof(EATT), "EscapeAtThisTime", "0.0.2", "EstaticYT")]
